cb25b34c004213646a62a1ab0021bf46566c6ac8
SEOB <svj7531@naver.com>
2024-03-11 22:21:46 +0900

    feat: [BE] URL 상수 처리

---
+++ src/main/java/com/fx/knutNotice/config/KnutURL.java
@@ -0,0 +1,10 @@
+package com.fx.knutNotice.config;
+
+public class KnutURL {
+
+    static final String GENERAL_NEWS = "https://www.ut.ac.kr/cop/bbs/BBSMSTR_000000000059/selectBoardList.do"; //일반소식
+    static final String SCHOLARSHIP_NEWS = "https://www.ut.ac.kr/cop/bbs/BBSMSTR_000000000060/selectBoardList.do"; //장학안내
+    static final String EVENT_NEWS = "https://www.ut.ac.kr/cop/bbs/BBSMSTR_000000000061/selectBoardList.do"; //행사안내
+    static final String ACADEMIC_NEWS = "https://www.ut.ac.kr/cop/bbs/BBSMSTR_000000000055/selectBoardList.do"; //학사공지사항
+
+}
