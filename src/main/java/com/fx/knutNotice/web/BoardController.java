1d5e722d4b4138a035b9a82fdbfdaa940c44550e
SEOB <svj7531@naver.com>
2024-03-12 22:25:49 +0900

    feat: [BE] 일반,장학,행사,학사 공지에 대한 Controller 메서드 구현

--- src/main/java/com/fx/knutNotice/web/BoardController.java
+++ src/main/java/com/fx/knutNotice/web/BoardController.java
@@ -1,5 +1,8 @@
 package com.fx.knutNotice.web;
 
+import com.fx.knutNotice.domain.entity.AcademicNews;
+import com.fx.knutNotice.domain.entity.EventNews;
+import com.fx.knutNotice.domain.entity.ScholarshipNews;
 import com.fx.knutNotice.service.BoardService;
 import com.fx.knutNotice.domain.entity.GeneralNews;
 import lombok.RequiredArgsConstructor;
@@ -15,11 +18,30 @@ public class BoardController {
 
     private final BoardService boardService;
 
-    @GetMapping()
-    public ResponseEntity showBoardList() {
-        List<GeneralNews> generalNews = boardService.showBoardList();
+    @GetMapping("/general-news")
+    public ResponseEntity showGeneralNews() { //일반소식
+        List<GeneralNews> generalNews = boardService.showGeneralNews();
         return ResponseEntity.ok(generalNews);
     }
+
+    @GetMapping("/scholarship-news")
+    public ResponseEntity showScholarshipNews() { //장학안내
+        List<ScholarshipNews> scholarshipNews = boardService.showScholarshipNews();
+        return ResponseEntity.ok(scholarshipNews);
+    }
+
+    @GetMapping("/event-news")
+    public ResponseEntity showEventNews() { //행사안내
+        List<EventNews> eventNews = boardService.showEventNews();
+        return ResponseEntity.ok(eventNews);
+    }
+
+    @GetMapping("/academic-news") //학사공지사항
+    public ResponseEntity showAcademicNews() {
+        List<AcademicNews> academicNews = boardService.showAcademicNews();
+        return ResponseEntity.ok(academicNews);
+    }
+
 
 
 
