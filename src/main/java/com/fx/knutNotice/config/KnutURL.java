9ff3c979e5768d24e14cd6b173c838f7c7ddfd86
SEOB <svj7531@naver.com>
2024-03-29 23:49:30 +0900

    refactor: 열거형으로 변경

--- src/main/java/com/fx/knutNotice/config/KnutURL.java
+++ src/main/java/com/fx/knutNotice/config/KnutURL.java
@@ -1,10 +1,19 @@
 package com.fx.knutNotice.config;
 
-public class KnutURL {
+public enum KnutURL {
 
-    static final String GENERAL_NEWS = "https://www.ut.ac.kr/cop/bbs/BBSMSTR_000000000059/selectBoardList.do"; //일반소식
-    static final String SCHOLARSHIP_NEWS = "https://www.ut.ac.kr/cop/bbs/BBSMSTR_000000000060/selectBoardList.do"; //장학안내
-    static final String EVENT_NEWS = "https://www.ut.ac.kr/cop/bbs/BBSMSTR_000000000061/selectBoardList.do"; //행사안내
-    static final String ACADEMIC_NEWS = "https://www.ut.ac.kr/cop/bbs/BBSMSTR_000000000055/selectBoardList.do"; //학사공지사항
+    GENERAL_NEWS("https://www.ut.ac.kr/cop/bbs/BBSMSTR_000000000059/selectBoardList.do"), //일반소식
+    SCHOLARSHIP_NEWS("https://www.ut.ac.kr/cop/bbs/BBSMSTR_000000000060/selectBoardList.do"), //장학안내
+    EVENT_NEWS("https://www.ut.ac.kr/cop/bbs/BBSMSTR_000000000061/selectBoardList.do"), //행사안내
+    ACADEMIC_NEWS("https://www.ut.ac.kr/cop/bbs/BBSMSTR_000000000055/selectBoardList.do"); //학사공지사항
 
+    private final String url;
+
+    KnutURL(String url) {
+        this.url = url;
+    }
+
+    public String URL() {
+        return this.url;
+    }
 }
