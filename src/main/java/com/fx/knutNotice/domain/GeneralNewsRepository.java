8a06be2c5d1fce9c58b5964deccc3173477aef7e
SEOB <svj7531@naver.com>
2024-03-30 23:18:12 +0900

    perf: 주석 제거

--- src/main/java/com/fx/knutNotice/domain/GeneralNewsRepository.java
+++ src/main/java/com/fx/knutNotice/domain/GeneralNewsRepository.java
@@ -5,26 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GeneralNewsRepository extends JpaRepository<GeneralNews, Long> {
 
-
-/*
-    private final Map<Long, GeneralNews> store = new ConcurrentHashMap<>();
-
-    public void save(GeneralNews generalNews) {
-        store.put(generalNews.getNttId(), generalNews);
-    }
-
-    public void findByNttId(Long nttId) {
-        store.get(nttId);
-    }
-
-    public List<GeneralNews> findAll() {
-        return new ArrayList<>(store.values());
-    }
-
-    public void deleteBoard(Long nttId) {
-        if (store.containsKey(nttId)) {
-            store.remove(nttId);
-        }
-    }
- */
 }
