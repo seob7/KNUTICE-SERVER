3b433e3cac5a247a083eac8e58fb2c4e67e76c6f
ITSTIME <88642524+ITSTIME1@users.noreply.github.com>
2024-05-10 14:03:59 +0900

    refactor

--- src/main/java/com/fx/knutNotice/service/newsUpdateService/BaseNewsService.java
+++ src/main/java/com/fx/knutNotice/service/newsUpdateService/BaseNewsService.java
@@ -3,148 +3,94 @@ package com.fx.knutNotice.service.newsUpdateService;
 import com.fx.knutNotice.domain.*;
 import com.fx.knutNotice.domain.entity.BaseNews;
 import com.fx.knutNotice.dto.BoardDTO;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public abstract class BaseNewsService<T extends BaseNewsRepository> {
 
     private T repository;
-    private static Long ACADEMIC_MAX_NTT_ID = 0L;
-    private static Long GENERAL_MAX_NTT_ID = 0L;
-    private static Long SCHOLARSHIP_MAX_NTT_ID = 0L;
-    private static Long EVENT_MAX_NTT_ID = 0L;
-    private byte newCount = 0; // 2^8 = 256개
-    private List<String> fcmTitles;
-
+    private static Long ACADEMIC_MAX_NTTID = 0L;
+    private static Long GENERAL_MAX_NTTID = 0L;
+    private static Long SCHOLARSHIP_MAX_NTTID = 0L;
+    private static Long EVENT_MAX_NTTID = 0L;
+    private static Long COMPARE_MAX_NTTID = -1L;
     public BaseNewsService(T repository) {
         this.repository = repository;
     }
 
+    public List<String> newsCheck(List<BoardDTO> newList) {
+
+        List<String> titleList = new ArrayList<>();
 
 
-    /**
-     * 초기화 작업.
-     */
+        /**
+         * repository 인스턴스 체크.
+         */
+        checkRepositoryInstance();
 
-    private void init() {
+        //기존 데이터 false로
         repository.updateNewCheckToFalse();
-        fcmTitles = new ArrayList<>();
-    }
 
+        int newCount = 0;//새로운 글 개수
 
-    /**
-     * 새로운 게시글을 업데이트.
-     */
-    public List<String> newsCheck(final List<BoardDTO> newList) {
-        init();
-        transaction(newList);
-        after();
-        return fcmTitles;
-    }
+        for (BoardDTO boardDTO : newList) {
+            //DB의 nttId(최신글)보다 크롤링한 nttId가 큰 경우 신규게시글로 판단
+            if (boardDTO.getNttId() > COMPARE_MAX_NTTID) {
+                BaseNews newEntity = createEntity(boardDTO);
+                repository.save(newEntity);
+                newCount++;
 
-    /**
-     * 저장과 추가에 대한 하나의 트랜잭션
-     */
-    private void transaction(final List<BoardDTO> newList) {
-        final Long dbMaxNttID = getMaxNttId();
-        for (final BoardDTO boardDTO : newList) {
-            if (boardDTO.getNttId() > dbMaxNttID) {
-                saveNewsEntity(boardDTO);
-                addNewsTitle(boardDTO);
+                // FCM발송을 위해 업데이트된 제목만 LIST에 추가.
+                titleList.add(boardDTO.getTitle());
             }
         }
-    }
 
-    /**
-     * for 문에서 참조로 전달한 crawling news 객체.
-     */
-    private void saveNewsEntity(final BoardDTO boardDTO) {
+        COMPARE_MAX_NTTID = newList.get(0).getNttId();
 
-        BaseNews newEntity = createEntity(boardDTO);
-        repository.save(newEntity);
-        newCount++;
-    }
-
-    /**
-     * FCM 용 title 추출.
-     */
-    private void addNewsTitle(final BoardDTO boardDTO) {
-        fcmTitles.add(boardDTO.getTitle());
-    }
-
-
-    /**
-     * 가장 오래된 news를 newCount(새로운 뉴스)개수 만큼 삭제.
-     */
-    private void deleteOldestNews() {
+        //boardNumber가 가장 작은 데이터를 업데이트된 개수만큼 글 삭제
         for (int i = 0; i < newCount; i++) {
-            final Long minBoardNumber = repository.findMinBoardNumber();
+            Long minBoardNumber = repository.findMinBoardNumber();
             repository.deleteByBoardNumber(minBoardNumber);
         }
+
+        return titleList;
     }
 
-
-
-    /**
-     * DB에서 maxNttId를 쿼리.
-     * @return maxNttId
-     */
     private Long findMaxNttId() {
         Long maxNttId = repository.findMaxNttId();
         return maxNttId != null ? maxNttId : 0L;
     }
 
-
-
-
-    /**
-     * DB에서 매번 가지고 오지 않고, 특정 뉴스의 nttID가 0L인 경우만 쿼리.
-     * @return 뉴스 타입의 저장 nttId
-     */
     private Long checkMaxNttId(final byte type) {
         switch (type) {
             case 0:
-                if(ACADEMIC_MAX_NTT_ID == 0L) ACADEMIC_MAX_NTT_ID = findMaxNttId();
-                return ACADEMIC_MAX_NTT_ID;
+                if(ACADEMIC_MAX_NTTID == 0L) ACADEMIC_MAX_NTTID = findMaxNttId();
+                return ACADEMIC_MAX_NTTID;
             case 1:
-                if(GENERAL_MAX_NTT_ID == 0L) GENERAL_MAX_NTT_ID = findMaxNttId();
-                return GENERAL_MAX_NTT_ID;
+                if(GENERAL_MAX_NTTID == 0L) GENERAL_MAX_NTTID = findMaxNttId();
+                return GENERAL_MAX_NTTID;
             case 2:
-                if(EVENT_MAX_NTT_ID == 0L) EVENT_MAX_NTT_ID = findMaxNttId();
-                return EVENT_MAX_NTT_ID;
+                if(EVENT_MAX_NTTID == 0L) EVENT_MAX_NTTID = findMaxNttId();
+                return EVENT_MAX_NTTID;
             case 3:
-                if(SCHOLARSHIP_MAX_NTT_ID == 0L) SCHOLARSHIP_MAX_NTT_ID = findMaxNttId();
-                return SCHOLARSHIP_MAX_NTT_ID;
+                if(SCHOLARSHIP_MAX_NTTID == 0L) SCHOLARSHIP_MAX_NTTID = findMaxNttId();
+                return SCHOLARSHIP_MAX_NTTID;
         }
         return -1L;
     }
 
-
-
-    /**
-     * 인스턴스 타입에 따라 가장 큰 nttId를 반환.
-     * @return 특정 뉴스의 maxNttId
-     */
-    private Long getMaxNttId (){
+    private void checkRepositoryInstance (){
         if(repository instanceof AcademicNewsRepository) {
-            return checkMaxNttId((byte) 0);
+            COMPARE_MAX_NTTID = checkMaxNttId((byte) 0);
         } else if( repository instanceof GeneralNewsRepository) {
-            return checkMaxNttId((byte) 1);
+            COMPARE_MAX_NTTID = checkMaxNttId((byte) 1);
         } else if( repository instanceof EventNewsRepository) {
-            return checkMaxNttId((byte) 2);
+            COMPARE_MAX_NTTID = checkMaxNttId((byte) 2);
         } else if(repository instanceof ScholarshipNewsRepository){
-            return checkMaxNttId((byte) 3);
+            COMPARE_MAX_NTTID = checkMaxNttId((byte) 3);
         }
-        return 0L;
     }
 
-    public void after () {
-        deleteOldestNews();
-    }
-
-
-    /**
-     * 상속 받는 클래스에서, 엔티티를 생성.
-     */
     protected abstract BaseNews createEntity(BoardDTO boardDTO);
 }
