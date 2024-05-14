0c9736c62f6d92a9fe3e95b390c7c9518f577156
SEOB <svj7531@naver.com>
2024-05-14 21:57:01 +0900

    feat: 게시글 상세 기능 Controller 추가

--- src/main/java/com/fx/knutNotice/web/BoardDetailController.java
+++ src/main/java/com/fx/knutNotice/web/BoardDetailController.java
@@ -1,5 +1,6 @@
 package com.fx.knutNotice.web;
 
+import com.fx.knutNotice.common.ResponseMessage;
 import com.fx.knutNotice.dto.BoardDTO;
 import com.fx.knutNotice.service.BoardDetailService;
 import com.fx.knutNotice.web.form.ResultForm;
@@ -15,9 +16,28 @@ import org.springframework.web.bind.annotation.RestController;
 public class BoardDetailController {
 
     private final BoardDetailService boardDetailService;
+
     @GetMapping("/generalNews/{nttId}")
     public ResultForm showGeneralNewsDetail(@PathVariable Long nttId) {
         BoardDTO boardDTO = boardDetailService.showGeneralNewsDetail(nttId);
-        return ResultForm.success("성공", boardDTO);
+        return ResultForm.success(ResponseMessage.SUCCESS_GENERAL_NEWS.getDescription(), boardDTO);
+    }
+
+    @GetMapping("/scholarshipNews/{nttId}")
+    public ResultForm showScholarshipNewsDetail(@PathVariable Long nttId) {
+        BoardDTO boardDTO = boardDetailService.showScholarshipNewsDetail(nttId);
+        return ResultForm.success(ResponseMessage.SUCCESS_SCHOLARSHIP_NEWS.getDescription(), boardDTO);
+    }
+
+    @GetMapping("/eventNews/{nttId}")
+    public ResultForm showEventNewsDetail(@PathVariable Long nttId) {
+        BoardDTO boardDTO = boardDetailService.showEventNewsDetail(nttId);
+        return ResultForm.success(ResponseMessage.SUCCESS_EVENT_NEWS.getDescription(), boardDTO);
+    }
+
+    @GetMapping("/academicNews/{nttId}")
+    public ResultForm showAcademicNewsDetail(@PathVariable Long nttId) {
+        BoardDTO boardDTO = boardDetailService.showAcademicNewsDetail(nttId);
+        return ResultForm.success(ResponseMessage.SUCCESS_ACADEMIC_NEWS.getDescription(), boardDTO);
     }
 }
