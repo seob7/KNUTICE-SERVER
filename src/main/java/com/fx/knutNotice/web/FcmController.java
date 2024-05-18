ecd9b335b561098aa68cf3c4cb32de56925cd710
SEOB <svj7531@naver.com>
2024-05-19 02:36:43 +0900

    feat: FcmController swagger 구현

--- src/main/java/com/fx/knutNotice/web/FcmController.java
+++ src/main/java/com/fx/knutNotice/web/FcmController.java
@@ -7,6 +7,9 @@ import com.fx.knutNotice.service.FcmService;
 import com.fx.knutNotice.dto.DeviceTokenDTO;
 import com.fx.knutNotice.web.form.ResultForm;
 import com.google.firebase.messaging.FirebaseMessagingException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -19,12 +22,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/fcm")
+@Tag(name = "FCM Controller", description = "토큰 저장 및 관리 API")
 public class FcmController {
 
     private final FcmService fcmService;
     private final DeviceTokenService deviceTokenService;
 
-    @PostMapping("/sendAllMessage")
+//    @PostMapping("/sendAllMessage")
     public ResultForm pushMessage(@RequestBody FcmDTO requestDTO)
             throws FirebaseMessagingException {
 
@@ -33,12 +37,16 @@ public class FcmController {
     }
 
     @GetMapping("/getAllTokens")
+    @Operation(summary = "모든 토큰 조회", description = "요청시 모든 토큰 조회 가능")
+    @ApiResponse(responseCode = "200", description = "전체 토큰 조회 성공")
     public ResultForm getAllTokens() {
         return ResultForm.success(FcmMessage.SUCCESS_ALL_TOKENS_RETRIEVED.getDescription(),
                 deviceTokenService.getAllTokens());
     }
 
     @PostMapping("/sendDeviceToken")
+    @Operation(summary = "토큰 저장", description = "id는 기기 고유 식별번호, deviceToken은 FCM의 토큰 번호입니다.")
+    @ApiResponse(responseCode = "200", description = "Device Token 저장 성공")
     public ResultForm sendDeviceToken(@RequestBody DeviceTokenDTO deviceTokenDTO) {
         deviceTokenService.saveDeviceToken(deviceTokenDTO);
         return ResultForm.success(FcmMessage.SUCCESS_TOKEN_REGISTRATION.getDescription());
