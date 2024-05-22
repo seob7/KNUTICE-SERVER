07bfa428375561f6150a43d905a95dcecf8a3cc6
SEOB <svj7531@naver.com>
2024-05-23 03:12:04 +0900

    refactor: FcmTrigger 수정

--- src/main/java/com/fx/knutNotice/worker/BoardUpdateWorker.java
+++ src/main/java/com/fx/knutNotice/worker/BoardUpdateWorker.java
@@ -2,7 +2,9 @@ package com.fx.knutNotice.worker;
 
 import com.fx.knutNotice.dto.BoardDTO;
 import com.fx.knutNotice.factory.ServiceFactory;
+import com.fx.knutNotice.service.FcmService;
 import com.fx.knutNotice.service.newsUpdateService.*;
+import com.google.firebase.messaging.FirebaseMessagingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,8 +14,13 @@ import java.util.List;
 @RequiredArgsConstructor
 public class BoardUpdateWorker {
     private final ServiceFactory serviceFactory;
-    public void updateBoardTitles (final List<BoardDTO> titles, final byte type){
+    private final FcmService fcmService;
+
+    public void updateBoardTitles (final List<BoardDTO> titles, final byte type)
+        throws FirebaseMessagingException {
         BaseNewsService baseNewsService = serviceFactory.getService(type);
-        baseNewsService.updateNews(titles, type).getUpdateTitles();
+        List updateTitles = baseNewsService.updateNews(titles, type).getUpdateTitles();
+
+        fcmService.fcmTrigger(updateTitles, type);
     }
 }
